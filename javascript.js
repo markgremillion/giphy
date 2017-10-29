630cc440fc9ff059cdf94e79a1ef8baf5dcc0732
mark gremillion <markgremillion@gmail.com>
2017-10-28 21:20:36 -0600

    second commit

--- javascript.js
+++ javascript.js
@@ -1,50 +1,71 @@
 $(function (){
-	populateButtons(searchArray, 'searchButton', '#buttonsArea');
+    populateButtons(searchArray, 'searchButton', '#buttonsArea');
 
 
 })
 
-	var searchArray = ["dog", "cat", "bird"];
+    var searchArray = ["dog", "cat", "bird", "cow"];
 
-	function populateButtons (searchArray, classToAdd, areaToAddTo){
-		$(areaToAddTo).empty();
-		for (var i=0; i<searchArray.length ; i++){
-			var a = $('<button>');
-			a.addClass(classToAdd);
-			a.attr('data-type', searchArray[i]);
-			a.text(searchArray[i]);
-			$(areaToAddTo).append(a);
+    function populateButtons (searchArray, classToAdd, areaToAddTo){
+        $(areaToAddTo).empty();
+        for (var i=0; i<searchArray.length ; i++){
+            var a = $('<button>');
+            a.addClass(classToAdd);
+            a.attr('data-type', searchArray[i]);
+            a.text(searchArray[i]);
+            $(areaToAddTo).append(a);
+            console.log(searchArray)
 
-		}
-	}
+        }
+    }
 
-	$(document).on('click', '.searchButton', function(){
-		$('#searches').empty();
-		var type = $(this).data('type');
-		var queryURL = 'http://api.gify.com/v1/gifs/search?q=' + type + '&api_key=A8a7K6gHY0QUE70tfxil7AszKFvlhobG'; //put in key
-		}); 
-			$.ajax({
-				url:queryURL, 
-				method:'GET',
-			}) 
-			.done(function(response){
-			for (var i=0; i<response.data.length;i++){
-				var searchDiv = $("<div class= 'search-item'>")
-				var rating = response.data[i].rating;
-				var p = $('<p>').text('Rating: ' +rating);
-				var animated = response.data[i].images.fixed_height.url;
-				var still = response.data[i].images.fixed_height_still.url;
-				var image = $("<img>");
-				image.attr('src',still);
-				image.attr('data-still', still);
-				image.arrt('data-animated', animated);
-				image.attr('data-state', 'still');
-				image.addClass('searchImage');
-				searchDiv.append(p);
-				searchDiv.append(image);
-				$('#searches').append(searchDiv);
 
-			} 
+    $(document).on('click', '.searchButton', function(){
+        $('#searches').empty();
+        var type = $(this).attr('data-type'); 
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=A8a7K6gHY0QUE70tfxil7AszKFvlhobG&limit=10";
 
-	})
+        
+        $.ajax({
+            url: queryURL,
+            method: 'GET',
+        })
+        .done(function(response){
+            for (var i=0; i<response.data.length;i++){
+                var searchDiv = $('<div class= "search-item">')
+                var rating = response.data[i].rating;
+                var p = $('<p>').text('Rating: ' +rating);
+                var animated = response.data[i].images.fixed_height.url;
+                var still = response.data[i].images.fixed_height_still.url;
+                var image = $("<img>");
+                image.attr('src',still);
+                image.attr('data-still', still);
+                image.attr('data-animated', animated);
+                image.attr('data-state', 'still');
+                image.addClass('search-image');
+                searchDiv.append(p);
+                searchDiv.append(image);
+                $('#searches').append(searchDiv);
+            }//
+        }); 
+    });
 	
+$(document).on('click', '.search-image', function(){
+	var state= $(this).attr('data-state');
+	if (state == 'still'){
+		$(this).attr('src', $(this).data('animated'));
+		$(this).attr('data-state', 'animated');
+	}   else {
+		$(this).attr('src', $(this).data('still'));
+		$(this).attr('data-state', 'still');
+	}
+})
+
+$('#addSearch').on('click', function(){
+	var newSearch = $('input').eq(0).val();
+		searchArray.push(newSearch); 
+		populateButtons(searchArray, 'searchButton', '#buttonsArea');
+		return false;
+
+
+});
